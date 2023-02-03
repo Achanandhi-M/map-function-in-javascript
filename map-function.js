ed407513a8b026fa4309d790a70b0006d821c196
Achanandhi.M <110651321+Achanandhi24@users.noreply.github.com>
2023-02-03 10:09:09 +0530

    Add files via upload

---
+++ map-function.js
@@ -0,0 +1,20 @@
+let arr=[3,4,5,6,7]
+let modifiedarr=arr.map(function(element){
+return element+3;
+});
+console.log(modifiedarr)
+console.log(typeof(modifiedarr))//Everything in javascript is considered as object
+
+
+
+
+// Array in javascript
+/*
+let a=[1,2,3,]
+let b=[1,4,5]
+let c;
+c=parseInt(a)+parseInt(b);
+console.log(c)
+console.log(typeof(a))
+console.log(typeof(c))
+*/
